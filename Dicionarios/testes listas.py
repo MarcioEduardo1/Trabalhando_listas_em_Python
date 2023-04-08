01ce033f606b977e30d9a57e8639fedfdb788be4
marcioeduardo1 <marcioeduardo147@gmail.com>
2023-04-08 15:49:47 -0300

    Solução

---
+++ Dicionarios/testes listas.py
@@ -0,0 +1,10 @@
+frutas = ["maça", "banana", "uva", "mamão", "maçã"]
+notas = [8.7, 5.2, 10, 3.5]
+
+print("maça" in frutas) # True
+print("abacate" in frutas) # false
+print("abacate" not in frutas)
+print(min(frutas))
+print(frutas.count("maça"))
+print(frutas + notas)
+print(2 * frutas)
